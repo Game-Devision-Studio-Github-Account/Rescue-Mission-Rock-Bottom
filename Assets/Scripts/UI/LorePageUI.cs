9b61dd9c370d067cebc19d0885243871f0a84170
Kris <itr45hm41l@gmail.com>
2023-11-01 18:30:20 -0400

    More Enemies and Damage

--- Assets/Scripts/UI/LorePageUI.cs
+++ Assets/Scripts/UI/LorePageUI.cs
@@ -1,17 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LorePageUI : MonoBehaviour
 {
     public KeyCode dismissInput = KeyCode.Space;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public Image noteImage;
 
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetKeyDown(dismissInput)) {
